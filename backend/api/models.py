d2babe908763cacc49cd37a9b27d473a2514cbe2
Allan <allanjaybusante3@gmail.com>
2025-12-12 22:49:11 +0800

    add expense and category model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+class Expense(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='expenses')
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    spent_at = models.DateTimeField()
+
+    class Meta:
+        ordering = ['-spent_at']
+
+    def __str__(self):
+        return f"{self.name} - ₱{self.amount}"
